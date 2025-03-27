c3eb0a649426387fa53f311b410b65f7e7f949bc
NavinkumarJ <navinkumar02032005@gmail.com>
2025-03-27 23:23:51 +0530

    Changed .env

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,22 @@
 const mysql = require("mysql2");
 require("dotenv").config();
+const mysql = require('mysql2');
 
-const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+const connection = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
-db.getConnection((err, connection) => {
-    if (err) {
-        console.error("Database connection failed:", err);
-        process.exit(1);
-    }
-    console.log("Connected to MySQL Database");
-    connection.release();
+connection.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err);
+  } else {
+    console.log('Connected to MySQL on Railway!');
+  }
 });
+
 
 module.exports = db;
