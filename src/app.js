407672e55e5b00cb72643bb6703d8d5a2a44eb23
NavinkumarJ <navinkumar02032005@gmail.com>
2025-03-28 00:46:51 +0530

    app.js-updated

--- src/app.js
+++ src/app.js
@@ -5,6 +5,16 @@ const schoolRoutes = require("./routes/schoolRoutes");
 const app = express();
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to the School Management API",
+        endpoints: {
+            addSchool: "/api/addSchool",
+            listSchools: "/api/listSchools?latitude=<value>&longitude=<value>"
+        }
+    });
+});
+
 app.use("/api", schoolRoutes);
 
 module.exports = app;
